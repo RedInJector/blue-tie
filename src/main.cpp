82036c8710e866ba65124f9a7ae79bdf195b4647
RedInJector <imaxprots@gmail.com>
2025-06-03 16:15:32 +0200

    moved to App class based approach

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,7 @@
 #include <fstream>
 #include <print>
 #include <random>
-#include <Windows.h>
-#include <shellapi.h>
 #include <thread>
-#include <tchar.h>
 
 #include "single_instance_guard.h"
 #include "tray_icon.h"
@@ -17,8 +14,8 @@
 std::unique_ptr<discord::Core> create_discord_core() {
     std::unique_ptr<discord::Core> core_p;
     discord::Core *core{};
-    auto result = discord::Core::Create(1378475123610353674,
-                                        DiscordCreateFlags_NoRequireDiscord, &core);
+    discord::Core::Create(1378475123610353674,
+                          DiscordCreateFlags_NoRequireDiscord, &core);
     if (core) {
         std::print("Instantiated discord core\n");
         core_p.reset(core);
@@ -27,18 +24,6 @@ std::unique_ptr<discord::Core> create_discord_core() {
     return core_p;
 }
 
-
-void set_activity(discord::Core &core, const std::string &text, const std::string &image_name = "beam") {
-    discord::Activity activity{};
-    activity.SetType(discord::ActivityType::Playing);
-
-    activity.SetDetails(text.c_str());
-    activity.GetAssets().SetLargeImage(image_name.c_str());
-    activity.SetInstance(true);
-
-    core.ActivityManager().UpdateActivity(activity, [](discord::Result result) {
-    });
-}
 
 std::vector<std::string> load_lines(const std::filesystem::path &path) {
     std::vector<std::string> lines;
@@ -64,74 +49,129 @@ std::vector<std::string> load_lines(const std::filesystem::path &path) {
     return lines;
 }
 
+class PseudoRandomGenerator {
+    std::mt19937 gen;
 
-int main(int argc, char *argv[]) {
-    auto guard = SingleInstanceGuard::Create();
-    if (!guard->IsPrimaryInstance()) {
-        std::print("Already running");
+public:
+    PseudoRandomGenerator() {
+        std::random_device rd;
+        std::vector<std::uint32_t> seed_data(10);
+        for (auto &val: seed_data) {
+            val = rd();
+        }
+        std::seed_seq seed(seed_data.begin(), seed_data.end());
+        gen.seed(seed);
+    }
+
+    explicit PseudoRandomGenerator(const unsigned int seed) {
+        gen.seed(seed);
+    }
+
+
+    int GetInt(const int min, const int max) {
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(gen);
+    }
+};
+
+class App {
+    PseudoRandomGenerator pseudo_random_generator_;
+
+    std::unique_ptr<SingleInstanceGuard> instance_guard_;
+    std::vector<std::string> quotes_;
+
+    int time_offset_min_ = 60;
+    int time_offset_max = 120;
+
+    std::unique_ptr<discord::Core> discord_core_;
+    std::unique_ptr<TrayIcon> tray_icon_;
+
+    std::chrono::time_point<std::chrono::system_clock> next_random_time_;
+    int64_t activity_start_time_;
+
+    bool run_flag_ = true;
+
+    void SetActivity(const std::string &text, const std::string &image_name = "beam") const {
+        discord::Activity activity{};
+        activity.SetType(discord::ActivityType::Playing);
+
+        activity.GetTimestamps().SetStart(activity_start_time_);
+        activity.SetDetails(text.c_str());
+        activity.GetAssets().SetLargeImage(image_name.c_str());
+
+        discord_core_->ActivityManager().UpdateActivity(activity, [](discord::Result result) {
+        });
+    }
+
+    void RerollTime() {
+        const int random_offset = pseudo_random_generator_.GetInt(time_offset_min_, time_offset_max);
+        next_random_time_ = std::chrono::system_clock::now() + std::chrono::seconds(random_offset);
+    }
+
+    void RollRandomActivity() {
+        RerollTime();
+        int random_index = pseudo_random_generator_.GetInt(0, quotes_.size() - 1);
+        SetActivity(quotes_.at(random_index));
+    }
+
+public:
+    App() : instance_guard_(SingleInstanceGuard::Create()), quotes_(load_lines("data.tie")) {
+        if (quotes_.empty())
+            quotes_.emplace_back("blue hair, blue tie");
+
+        activity_start_time_ = std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now().time_since_epoch()
+        ).count();
+
+        RerollTime();
+    }
+
+
+    int run() {
+        if (!instance_guard_->IsPrimaryInstance()) {
+            std::print("Already running");
+            return 1;
+        }
+        tray_icon_ = TrayIcon::Create();
+
+        tray_icon_->SetToolTip("Blue Tie");
+        tray_icon_->AddButton("Exit", [this]() {
+            run_flag_ = false;
+        });
+        tray_icon_->AddButton("Re-roll", [this]() {
+            RollRandomActivity();
+        });
+
+        tray_icon_->Show();
+
+        while (run_flag_) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(30));
+            if (!discord_core_) {
+                discord_core_ = create_discord_core();
+                if (discord_core_) {
+                    RollRandomActivity();
+                }
+            }
+            if (discord_core_) {
+                auto time_now = std::chrono::system_clock::now();
+                if (time_now >= next_random_time_) {
+                    RollRandomActivity();
+                }
+                auto res = discord_core_->RunCallbacks();
+                if (res != discord::Result::Ok) {
+                    discord_core_.reset();
+                }
+            }
+
+            tray_icon_->ProcessEvents();
+        }
+
         return 0;
     }
-
-    auto quotes = load_lines("data.tie");
-    if (quotes.empty())
-        quotes.emplace_back("blue hair, blue tie");
-
-    bool run = true;
-    bool reroll = false;
-
-    const auto tray_icon = TrayIcon::Create();
-    tray_icon->SetToolTip("Blue Tie");
-    tray_icon->AddButton("Exit", [&run]() {
-        run = false;
-    });
-    tray_icon->AddButton("Re-roll", [&reroll]() {
-        reroll = true;
-    });
-
-    tray_icon->Show();
-
-    std::unique_ptr<discord::Core> core;
-
-    int min_sec = 20, max_sec = 60;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist(min_sec, max_sec);
-    std::uniform_int_distribution<int> line_index_dist(0, quotes.size() - 1);
-
-    int random_offset = dist(gen);
-
-    auto next_random_time = std::chrono::system_clock::now() + std::chrono::seconds(random_offset);
-
-    while (run) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(30));
-        if (!core) {
-            core = create_discord_core();
-            if (core) {
-                set_activity(*core, quotes.at(line_index_dist(gen)));
-            }
-        }
-        if (core) {
-            auto time_now = std::chrono::system_clock::now();
-            if (time_now >= next_random_time) {
-                set_activity(*core, quotes.at(line_index_dist(gen)));
-                random_offset = dist(gen);
-                next_random_time = time_now + std::chrono::seconds(random_offset);
-            }
-            auto res = core->RunCallbacks();
-            if (res != discord::Result::Ok) {
-                core.reset();
-            }
-            if (reroll) {
-                set_activity(*core, quotes.at(line_index_dist(gen)));
-                random_offset = dist(gen);
-                next_random_time = time_now + std::chrono::seconds(random_offset);
-                reroll = false;
-            }
-        }
-
-        tray_icon->ProcessEvents();
-    }
+};
 
 
-    return 0;
+int main(int argc, char *argv[]) {
+    App app;
+    return app.run();
 }
